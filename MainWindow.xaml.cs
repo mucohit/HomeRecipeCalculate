30d0bd4eba5a5e17365a94e35dba245c2b3d858b
mucahit aric <aricmucahit@gmail.com>
2020-11-02 01:15:51 -0500

    The Home Recipe Project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Recipe_Projects
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        People p1 = new People { Name = "Ahmet", Amount = 0, NumberOfRecipe=0 };
+        People p2 = new People { Name = "Cihat", Amount = 0, NumberOfRecipe = 0 };
+        People p3 = new People { Name = "Muhammed", Amount = 0, NumberOfRecipe = 0 };
+        People p4 = new People { Name = "Mucahit", Amount = 0, NumberOfRecipe = 0 };
+        People p5 = new People { Name = "Yusuf", Amount = 0, NumberOfRecipe = 0 };
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            cmbPerson.Items.Add(p1.Name);
+            cmbPerson.Items.Add(p2.Name);
+            cmbPerson.Items.Add(p3.Name);
+            cmbPerson.Items.Add(p4.Name);
+            cmbPerson.Items.Add(p5.Name);
+        }
+
+        private void btnAddRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            People people = new People();
+            if(cmbPerson.SelectedItem.ToString() == p1.Name)
+            {
+                p1.Amount += Convert.ToDouble(txtAmount.Text);
+                p1.NumberOfRecipe++;
+            }
+            if(cmbPerson.SelectedItem.ToString() == p2.Name)
+            {
+                p2.Amount += Convert.ToDouble(txtAmount.Text);
+                p2.NumberOfRecipe++;
+            }
+            if (cmbPerson.SelectedItem.ToString() == p3.Name)
+            {
+                p3.Amount += Convert.ToDouble(txtAmount.Text);
+                p3.NumberOfRecipe++;
+            }
+            if (cmbPerson.SelectedItem.ToString() == p4.Name)
+            {
+                p4.Amount += Convert.ToDouble(txtAmount.Text);
+                p4.NumberOfRecipe++;
+            }
+            if (cmbPerson.SelectedItem.ToString() == p5.Name)
+            {
+                p5.Amount += Convert.ToDouble(txtAmount.Text);
+                p5.NumberOfRecipe++;
+            }
+            txtAmount.Text = "0.00$";
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            Recipe recipe = new Recipe(p1.Amount, p2.Amount, p3.Amount, p4.Amount, p5.Amount);
+            recipe.Calculate();
+
+            lblAhmet.Content = recipe.Ahmet.ToString("c");
+            lblCihat.Content = recipe.Cihat.ToString("c");
+            lblYusuf.Content = recipe.Yusuf.ToString("c");
+            lblMuhammed.Content = recipe.Muhammed.ToString("c");
+            lblMucahit.Content = recipe.Mucahit.ToString("c");
+
+        }
+    }
+}
---
+++ Recipe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Projects
+{
+    public class Recipe
+    {
+        public double Ahmet { get; set; }
+        public double Cihat { get; set; }
+        public double Muhammed { get; set; }
+        public double Mucahit { get; set; }
+        public double Yusuf { get; set; }
+        public double Total { get; set; }
+
+        public Recipe(double p1,double p2, double p3, double p4, double p5)
+        {
+            Ahmet = p1;
+            Cihat = p2;
+            Muhammed=p3;
+            Mucahit= p4;
+            Yusuf = p5;
+        }
+
+        public void Calculate()
+        {
+            Total = Ahmet + Yusuf + Cihat + Muhammed + Mucahit;
+            Ahmet = Ahmet - (Total / 5);
+            Yusuf = Yusuf - (Total / 5);
+            Cihat = Cihat- (Total / 5);
+            Mucahit = Mucahit - (Total / 5);
+            Muhammed = Muhammed- (Total / 5);
+
+        }
+    }
+}
